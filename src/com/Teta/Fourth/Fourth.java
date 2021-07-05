c28b3d97d6d8adb192caa901f2d44f65a1da7e12
Jhinchuriki <86767695+Jhinchuriki@users.noreply.github.com>
2021-07-05 10:11:58 +0300

    Arrays

---
+++ src/com/Teta/Fourth/Fourth.java
@@ -0,0 +1,40 @@
+package com.Teta.Fourth;
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Fourth {
+    public static void main(String[] args) {
+        Ex1 exercise1 = new Ex1();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Write the number of the array length");
+        int l = sc.nextInt();
+        int[] arr = new int[l];
+        exercise1.FillArray(arr);
+        exercise1.SumTheArray(arr);
+
+
+
+    }
+
+    static class Ex1{
+        public void FillArray(int[] b){
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Write the numbers to fill the array");
+            String text = sc.nextLine();
+            String[] res = text.split("[,]", 0);
+            for(int a= 0;a<res.length;a++){
+                b[a]= Integer.parseInt(res[a]);
+            }
+
+        }
+
+        public static void SumTheArray(int[] b){
+            int m = 0;
+            for(int i = 0; i<b.length;i++){
+                m=m+b[i];
+            }
+            System.out.println(m);
+        }
+    }
+
+}
