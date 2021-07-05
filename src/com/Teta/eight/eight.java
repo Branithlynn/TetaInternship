6e30ef9dc87b3b16b51d868d1eb5c5fe42494a9f
Jhinchuriki <86767695+Jhinchuriki@users.noreply.github.com>
2021-07-05 14:30:18 +0300

    Updated String names

--- src/com/Teta/eight/eight.java
+++ src/com/Teta/eight/eight.java
@@ -5,15 +5,15 @@ import java.util.Scanner;
 public class eight {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[] t1 = sc.nextLine().split(" ");
-        String[] t2 = sc.nextLine().split(" ");
-        equality(t1,t2);
+        String[] input1 = sc.nextLine().split(" ");
+        String[] input2 = sc.nextLine().split(" ");
+        equality(input1,input2);
     }
-    public static void equality(String[] t1, String[] t2){
-        for(int i = 0; i<t1.length;i++){
-            for(int k = 0; k<t1.length;k++){
-               if(t1[i].equals(t2[k])){
-                   System.out.print(t1[i]+ " ");
+    public static void equality(String[] input1, String[] input2){
+        for(int i = 0; i<input2.length;i++){
+            for(int k = 0; k<input1.length;k++){
+               if(input2[i].equals(input1[k])){
+                   System.out.print(input2[i]+ " ");
                }
             }
         }
