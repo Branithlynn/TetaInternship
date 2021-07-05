bc0eaf6c1cd7dd9bc4667b57e2782ad7042d992d
Jhinchuriki <86767695+Jhinchuriki@users.noreply.github.com>
2021-07-05 14:24:33 +0300

    Updated string splitters

--- src/com/Teta/eight/eight.java
+++ src/com/Teta/eight/eight.java
@@ -7,9 +7,9 @@ public class eight {
         Scanner sc = new Scanner(System.in);
         String[] t1 = sc.nextLine().split(" ");
         String[] t2 = sc.nextLine().split(" ");
-        Equality(t1,t2);
+        equality(t1,t2);
     }
-    public static void Equality(String[] t1, String[] t2){
+    public static void equality(String[] t1, String[] t2){
         for(int i = 0; i<t1.length;i++){
             for(int k = 0; k<t1.length;k++){
                if(t1[i].equals(t2[k])){
